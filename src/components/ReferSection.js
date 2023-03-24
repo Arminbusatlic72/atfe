046ca85aca89024b1930abf74bfc498770140e3b
Zorica Janjic <zorica.janjic@uk.kps.com>
2023-03-24 19:03:30 +0100

    Updating button

--- src/components/ReferSection.js
+++ src/components/ReferSection.js
@@ -2,7 +2,12 @@ import Button from "./Button";
 function ReferSection() {
   return (
     <>
-      <Button color="primary" label="View Job" type="internal" url="/" />
+      <Button
+        color="primary"
+        label="View Job"
+        url="/"
+        customClass="card__button"
+      />
       <Button color="secondary" label="View Job" type="internal" url="/" />
       <Button color="tertiary" label="View Job" type="internal" url="/" />
     </>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -8,11 +8,11 @@ const Slider = ({ slides }) => {
     width: "100%",
     display: "flex",
     position: "relative",
-    border: "1px solid black"
+    border: "1px solid black",
   };
   const slideStyles = {
     height: "100%",
-    width: "33%"
+    width: "33%",
   };
   const leftArrowStyles = {
     position: "absolute",
@@ -22,7 +22,7 @@ const Slider = ({ slides }) => {
     fontSize: "45px",
     zIndex: "1",
     cursor: "pointer",
-    color: "black"
+    color: "black",
   };
   const rightArrowStyles = {
     position: "absolute",
@@ -32,7 +32,7 @@ const Slider = ({ slides }) => {
     fontSize: "45px",
     zIndex: "1",
     cursor: "pointer",
-    color: "black"
+    color: "black",
   };
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
@@ -131,6 +131,19 @@ const Slider = ({ slides }) => {
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 card__button"
           >
             Read more
+            <svg
+              aria-hidden="true"
+              class="w-4 h-4 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </a>
         </div>
       ))}
