a5ac5bbd13797120682135b4c937bdb4145eb264
Zorica Janjic <zorica.janjic@uk.kps.com>
2023-03-26 03:23:28 +0200

    Styling About Teaching Jobs Section

--- src/components/AboutTeachingJobsSection.js
+++ src/components/AboutTeachingJobsSection.js
@@ -3,8 +3,8 @@ function AboutTeachingJobs() {
   return (
     <section className="bg-gray">
       <div className="container mx-auto px-4 pt-0">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="md:col-span-1 bg-gray-100 p-4 pt-0">
+        <div className="flex gap-4 flex-col md:flex-row">
+          <div className="p-4 pt-0">
             <Image
               src="/about-teaching-jobs.png"
               width={500}
@@ -12,17 +12,17 @@ function AboutTeachingJobs() {
               className="relative -bottom-11"
             />
           </div>
-          <div className="md:col-span-1 bg-gray-200 p-4 flex flex-col justify-center">
-            <h3 className="text-red-100 py-6 text-4xl  mb-5 font-bold font-headings">
-              Are you passionate about the education and looking for rewarding
+          <div className="p-4 flex flex-col justify-center teaching-jobs-section__about-right">
+            <h3 className="text-red-100 py-3 mb-5 font-200 font-headings font-bold teaching-jobs-section__about-header">
+              Are you passionate about education and looking for a rewarding
               career in teaching?
             </h3>
-            <p>
-              Look no further! We’re here to help you navigate the challenging
-              job market and find the perfect teaching job that suits your
-              qualifications and interests.
+            <p className="font-headings pb-8 font-light text-lg lg:text-2xl">
+              <span class="font-bold">Look no further!</span> We're here to help
+              you navigate the challenging job market and find the perfect
+              teaching job that suits your qualifications and interests.
             </p>
-            <p>
+            <p className="font-headings font-light text-lg lg:text-2xl pb-8">
               Here at Accelerate Teaching, we connect aspiring teachers with
               schools, colleges, and other educational institutions in Leeds and
               the surrounding areas.
